ad3e380d2ecc0a9c552531f884a0e6ed2b0d7e28
23DP1MKors <166382241+Maiz333@users.noreply.github.com>
2024-10-08 07:25:40 +0000

    Star sign (4 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,20 +7,59 @@ import java.util.Scanner;
 public class App
 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Give a number");
-        int number1 = Integer.valueOf(scanner.nextLine());
-        int number2 = Integer.valueOf(scanner.nextLine());
-        divisibleByThreeInRange(number1,number2);
+        printStars(5);
+        printStars(3);
+        printStars(9);
+        printSquare(4);
+        printRectangle(17,3);
+        printTriangle(4);
     }
-    public static void divisibleByThreeInRange(int number1, int number2) {
-        for (int i = number1; i <number2+1; i++) {
-            if(i%3==0) {
-                System.out.println(i);
+    public static void printStars(int number) {
+        int i = 1;
+        while (i<number) {
+                System.out.print("*");
+                i++;
+            }
+            System.out.println();
+     }
+        public static void printSquare(int size) {
+            System.out.println();
+            int i = 1;
+            while (i-1<size) {
+                System.out.print("****");
+                System.out.println();
+                i++; 
             }
         }
+        public static void printRectangle(int width, int height) {
+            System.out.println();
+            
+            int a=1;
+            while(a-1<height) { 
+                int i = 1;
+                while (i<width) {
+                    System.out.print("*");
+                    i++; 
+            }
+            System.out.println();
+            a++;
+        }
     }
- }
+    public static void printTriangle(int size) {
+        System.out.println();
+        int i = 1;
+        while(i-1<size+1){
+            int a = 1;
+            while(a<i){
+                System.out.print("*");
+            a++; 
+            }
+            System.out.println();
+            i++; 
+        }
+        
+    }
+}
 
     
     
