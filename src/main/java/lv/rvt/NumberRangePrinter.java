1cb8d8bd95178101a94b36d1ae66984c9838f96b
23DP1MKors <166382241+Maiz333@users.noreply.github.com>
2024-10-22 09:15:43 +0000

    Print in range

--- src/main/java/lv/rvt/NumberRangePrinter.java
+++ src/main/java/lv/rvt/NumberRangePrinter.java
@@ -1,37 +1,31 @@
 
 package lv.rvt;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class NumberRangePrinter {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        List<Integer> numbers = new ArrayList<>();
-
-       
-        System.out.println("Enter numbers (enter -1 to stop):");
-        while (true) {
-            int input = scanner.nextInt();
-            if (input == -1) {
-                break;
-            }
-            numbers.add(input);
-        }
-
     
-        System.out.println("From where? ");
-        int startIndex = scanner.nextInt();
-        System.out.println("To where? ");
-        int endIndex = scanner.nextInt();
-
-       
-        System.out.println("Numbers in the range:");
-        for (int i = startIndex; i <= endIndex; i++) {
-            System.out.println(numbers.get(i));
+    public static void printNumbersInRange(ArrayList<Integer> numbers, int lowerLimit, int upperLimit) {
+        for (int number : numbers) {
+            if (number >= lowerLimit && number <= upperLimit) {
+                System.out.println(number);
+            }
         }
+    }
 
-        scanner.close();
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(-1);
+        numbers.add(5);
+        numbers.add(1);
+
+        System.out.println("The numbers in the range [0, 5]");
+        printNumbersInRange(numbers, 0, 5);
+
+        System.out.println("The numbers in the range [3, 10]");
+        printNumbersInRange(numbers, 3, 10);
     }
 }
 
