220736f8d142b367283f2cbe8569f8b3af7d0490
23DP1MKors <166382241+Maiz333@users.noreply.github.com>
2024-11-06 14:09:57 +0000

    Statistics 3

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -1,14 +1,21 @@
 package lv.rvt;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Statistics statistics = new Statistics();
-        statistics.addNumber(3);
-        statistics.addNumber(5);
-        statistics.addNumber(1);
-        statistics.addNumber(2);
-        System.out.println("Count: " + statistics.getCount());
+
+        System.out.println("Enter numbers:");
+
+        while (true) {
+            int number = scanner.nextInt();
+            if (number == -1) {
+                break;
+            }
+            statistics.addNumber(number);
+        }
+
         System.out.println("Sum: " + statistics.sum());
-        System.out.println("Average: " + statistics.average());
     }
 }
