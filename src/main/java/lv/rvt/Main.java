d886345cf8a399d777ac9bd9bba28d6d8db92b30
23DP1MKors <166382241+Maiz333@users.noreply.github.com>
2024-11-06 14:14:22 +0000

    Statistics 4

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Statistics statistics = new Statistics();
+        
+        Statistics totalStatistics = new Statistics();
+        Statistics evenStatistics = new Statistics();
+        Statistics oddStatistics = new Statistics();
 
         System.out.println("Enter numbers:");
 
@@ -13,9 +16,19 @@ public class Main {
             if (number == -1) {
                 break;
             }
-            statistics.addNumber(number);
+    
+            totalStatistics.addNumber(number);
+            
+            if (number % 2 == 0) {
+                evenStatistics.addNumber(number);
+            } else {
+                oddStatistics.addNumber(number);
+            }
         }
 
-        System.out.println("Sum: " + statistics.sum());
+
+        System.out.println("Sum: " + totalStatistics.sum());
+        System.out.println("Sum of even numbers: " + evenStatistics.sum());
+        System.out.println("Sum of odd numbers: " + oddStatistics.sum());
     }
 }
