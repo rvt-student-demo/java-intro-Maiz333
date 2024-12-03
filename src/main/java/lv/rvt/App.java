6ba608c028d5e923eceb8e757600f10ec84c1958
23DP1MKors <166382241+Maiz333@users.noreply.github.com>
2024-12-03 08:21:47 +0000

    project fix

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,26 +2,18 @@
 package lv.rvt;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.nio.channels.Pipe.SourceChannel;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
-import org.w3c.dom.css.Counter;
-public class App
-{
-public static void main(String[] args) throws Exception {
-        Timer timer = new Timer();
+public class App {
+        public static void main(String[] args) throws Exception {
 
-while (true) {
-        System.out.println(timer);
-        timer.advance();
+                BufferedReader reader = Helper.getReader(filename:"persons.csv");
 
-        try {
-                Thread.sleep(10);
-        } catch (Exception e) {
+                reader.readLine();
+                String line;
 
+                while ((line = reader.readLine()) != null) {
+                        System.out.println(line);
+                }
         }
-}
-}
 }
